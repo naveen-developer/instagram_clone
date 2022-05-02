73ff633f0508930b6a472cd24c36977c0d331768
Naveen <naveenofficial100@gmail.com>
2022-05-02 08:52:47 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Profile from "./Profile";
+import Appheader from "./Appheader";
+import Createpost from "./Createpost";
+import Signup from "./Signup";
+import Signin from "./Signin";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route exact path="/signup" element={<Signup />} />
+
+        <Route exact path="/signin" element={<Signin />} />
+
+        <Route
+          exact
+          path="/"
+          element={
+            <>
+              <Appheader />
+              <App />
+            </>
+          }
+        />
+
+        <Route
+          exact
+          path="/profile"
+          element={
+            <>
+              <Appheader />
+              <Profile />
+            </>
+          }
+        />
+
+        <Route
+          exact
+          path="/createpost"
+          element={
+            <>
+              <Appheader />
+              <Createpost />
+            </>
+          }
+        />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
